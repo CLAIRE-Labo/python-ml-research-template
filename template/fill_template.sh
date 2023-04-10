f381d0a9177dccb83917015b154e99cd4cfc66a3
Skander Moalla <skan.moalla@gmail.com>
2023-04-10 19:54:20 +0200

    prepare to build.

--- template/fill_template.sh
+++ template/fill_template.sh
@@ -3,8 +3,10 @@
 source template/template_variables.sh
 
 mv "src/package_name/" "src/${PACKAGE_NAME}"
+
 OSX64_DIR="installation/osx-arm64"
-AMD_64_DIR="installation/amd64"
+AMD64_DIR="installation/amd64"
+
 for file in \
   "$OSX64_DIR/update_env_file.sh"\
   "$AMD64_DIR/dependencies/update_env_file.sh"\
