60c9fb06df59929550f2258731b3fdf32d97c149
Skander Moalla <skan.moalla@gmail.com>
2023-04-12 10:51:39 +0200

    Pre build login shell. (Previous was with conda run).

--- installation/amd64/EPFL_runai_setup/runai-submit-example.sh
+++ installation/amd64/EPFL_runai_setup/runai-submit-example.sh
@@ -4,10 +4,12 @@
 # Read some of the variables from the .env file.
 source ../.env
 
+PROJECT_NAME="<project-name>"
+
 runai submit \
   --name sandbox-home \
   --interactive \
-  --image ${LAB_NAME}/<project-name>:${USR} \
+  --image ${LAB_NAME}/ \
   --pvc runai-mlo-moalla-mlodata1:/mlodata1 \
   --pvc runai-mlo-moalla-mloraw1:/mloraw1 \
   --environment EPFL_RUNAI=1 \
@@ -15,5 +17,5 @@ runai submit \
   --environment DATA_DIR_IN_NFS=/mlodata1/${USER}/data/${PROJECT_NAME} \
   --environment LOGS_DIR_IN_NFS=/mlodata1/${USER}/logs/${PROJECT_NAME} \
   --environment PYCHARM_IDE_LOCATION=/mlodata1/${USER}/remote_development/pycharm \
-  --environment WANDB_API_KEY=${WANDB_API_KEY}  \
-  -- sleep infinity
+  --environment WANDB_API_KEY=${WANDB_API_KEY} \
+  -- sleep infinity <project-name >:${USR}
--- template/fill_template.sh
+++ template/fill_template.sh
@@ -8,16 +8,16 @@ OSX64_DIR="installation/osx-arm64"
 AMD64_DIR="installation/amd64"
 
 for file in \
-  "$OSX64_DIR/update_env_file.sh"\
-  "$AMD64_DIR/dependencies/update_env_file.sh"\
-  "$OSX64_DIR/README.md"\
-  "$AMD64_DIR/README.md"\
-  "$OSX64_DIR/environment.yml"\
-  "$AMD64_DIR/dependencies/environment.yml"\
-  "$AMD64_DIR/docker-compose.yml"\
-  "$AMD64_DIR/Makefile.yml"\
-  pyproject.toml
-do
+  pyproject.toml \
+  "$OSX64_DIR/README.md" \
+  "$OSX64_DIR/update_env_file.sh" \
+  "$AMD64_DIR/dependencies/environment.yml" \
+  "$AMD64_DIR/dependencies/update_env_file.sh" \
+  "$AMD64_DIR/EPFL_runai_setup/runai-submit-example.sh" \
+  "$AMD64_DIR/docker-compose.yml" \
+  "$AMD64_DIR/Makefile.yml" \
+  "$AMD64_DIR/README.md" \
+  "$OSX64_DIR/environment.yml"; do
   sed -i '' "s/<project-name>/${PROJECT_NAME}/g" $file
   sed -i '' "s/<package_name>/${PACKAGE_NAME}/g" $file
   sed -i '' "s/<python-version>/${PYTHON_VERSION}/g" $file
