12864ec14e237a1738516b56772ffd0d184944be
Skander Moalla <skan.moalla@gmail.com>
2023-04-17 17:27:04 +0200

    fix.

---
+++ template/README.md
@@ -0,0 +1,4 @@
+# TODO
+
+Some additional details about the template.
+Maybe move the Q&A here.
--- template/fill_template.sh
+++ template/fill_template.sh
@@ -8,15 +8,14 @@ OSX64_DIR="installation/osx-arm64"
 AMD64_DIR="installation/docker-amd64"
 
 for file in \
-  pyproject.toml \
+  "code/pyproject.toml" \
   "$OSX64_DIR/README.md" \
   "$OSX64_DIR/update_env_file.sh" \
   "$OSX64_DIR/environment.yml" \
   "$AMD64_DIR/dependencies/environment.yml" \
   "$AMD64_DIR/dependencies/update_env_file.sh" \
   "$AMD64_DIR/Makefile" \
-  "$AMD64_DIR/README.md" \
-  "src/${PACKAGE_NAME}/main.py"; do
+  "$AMD64_DIR/README.md"; do
   sed -i '' "s/<project-name>/${PROJECT_NAME}/g" "$file"
   sed -i '' "s/<package_name>/${PACKAGE_NAME}/g" "$file"
   sed -i '' "s/<python-version>/${PYTHON_VERSION}/g" "$file"
