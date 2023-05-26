e3c13dbd7fe63e1e8fdcd439ccdfbb72b92f905b
Skander Moalla <37197319+skandermoalla@users.noreply.github.com>
2023-05-26 22:56:35 +0100

    Update update_env_file.sh

--- installation/osx-arm64/update_env_file.sh
+++ installation/osx-arm64/update_env_file.sh
@@ -4,4 +4,4 @@ conda env export --no-builds --file "$OSX64_DIR/environment.yml"
 # Delete the path line.
 sed -i '' -e '$ d' "$OSX64_DIR/environment.yml"
 # Set the package to a local installation.
-sed -i '' 's$<project-name>==.*$ -e ../..$g' "$OSX64_DIR/environment.yml"
+sed -i '' 's$<project-name>==.*$-e ../..$g' "$OSX64_DIR/environment.yml"
