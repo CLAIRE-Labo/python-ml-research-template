b23c1486001a7b7c5b2f198f51b7d9542b3c31e4
Skander Moalla <skan.moalla@gmail.com>
2023-08-23 14:58:39 +0200

    fix back to cp.

--- installation/docker-amd64/entrypoint.sh
+++ installation/docker-amd64/entrypoint.sh
@@ -1,19 +1,27 @@
 # Halt in case of errors. https://gist.github.com/vncsna/64825d5609c146e80de8b1fd623011ca
 set -eo pipefail
-echo "Running entrypoint.sh"
+echo "[TEMPLATE] Running entrypoint.sh"
 
 # If the user sets EPFL_RUNAI=1, call the EPFL setup script.
 if [ -n "${EPFL_RUNAI}" ]; then
+  echo "[TEMPLATE] Running EPFL setup script."
   zsh "${EPFL_RUNAI_SETUP_DIR}"/setup.sh
 fi
 
 # W&B login.
 # This does not need an internet connection.
 if [ -n "${WANDB_API_KEY}" ]; then
+  echo "[TEMPLATE] Logging in to W&B."
   export WANDB_API_KEY="${WANDB_API_KEY}"
   wandb login "${WANDB_API_KEY}"
 fi
 
+# Install the package in editable mode.
+echo "[TEMPLATE] Installing the project."
+pip install -e "${PROJECT_DIR}"
+python -c "import ${PACKAGE_NAME}"
+
 # Exec so that the child process receives the OS signals.
 # It will be PID 1.
+echo "[TEMPLATE] Executing the command" "$@"
 exec "$@"
