b2c06ecf32bff34aa1435dec5bb6ffb5d1f1d74c
Skander Moalla <skander.moalla@epfl.ch>
2023-12-02 23:59:38 +0100

    [Docs] clarification about update-env-file.

--- installation/docker-amd64-cuda/from-python/update-env-file.sh
+++ installation/docker-amd64-cuda/from-python/update-env-file.sh
@@ -7,6 +7,7 @@
 ENV_FILE="${PROJECT_ROOT_AT}"/installation/docker-amd64-cuda/dependencies/requirements.txt
 # Export, but delete the package itself as it's installed at runtime.
 # This is because it is only available after mounting the code.
+# Also remove the details of all packages installed from files prefixed with @.
 pip freeze \
   | sed "/-e.*/d" \
   | sed "s/ @.*//g" \
